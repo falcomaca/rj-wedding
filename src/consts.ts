225d6e1140855c4261f62e47d3446ea599a1831d
Falconeti <falconeti@Falconeti-Macbook.local>
2026-02-19 00:26:13 +0100

    Galeria + slider + ajustes finales

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '✨ R&JL';
+export const SITE_TITLE = '✨ Rocío y José Luis';
 export const SITE_DESCRIPTION = '¡Nos Casamos! Aquí podrás encontrar toda la información sobre nuestra boda';
